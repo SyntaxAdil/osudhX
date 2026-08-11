48038970e96759f585bc99467b9fe2f52ad4f10b
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-08-11 15:31:19 +0600

    Add better-auth integration with user and session models, and update environment configuration

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -7,9 +7,11 @@ const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
   throw new Error("DATABASE_URL is not defined");
-}
+} 
+const clientUrl= process.env.PUBLIC_CLIENT_URL ||  "https://localhost:3000"
 
 export default {
   port,
   databaseUrl,
+  clientUrl
 };
---
+++ server/src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { jwt } from "better-auth/plugins";
+import { prisma } from "./prisma";
+import env from "../config/env";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: [env.clientUrl],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        input: false,
+        defaultValue: "customer",
+      },
+    },
+  },
+  plugins: [jwt()],
+});
