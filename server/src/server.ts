5019f4a68cde0cba2797a057a3c136ead6fd131b
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-08-12 13:34:05 +0600

    refactor: update server configuration and structure for improved deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,6 @@
-
-
 import app from "./app";
-import port from "../src/config/env";
+import env from "./config/env";
 
-
-// listen
-app.listen(port, () => {
-  console.log("Server is running.");
+app.listen(env.port, () => {
+  console.log(`Server is running on port ${env.port}`);
 });
